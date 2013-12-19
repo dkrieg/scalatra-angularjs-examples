e661b51de68063bd28d315c90a4c6ebb084617dd
Kate von Roeder <katevonroeder@gmail.com>
2013-12-19 13:53:08 -0800

    Update scalatra-angularjs-app to work with SBT 0.13.1 (latest).
    Updates all plugins to latest.
    Update js libs for test.
    Tested on win7 with jdk1.7.0_45

--- scalatra-angularjs-app/project/plugins.sbt
+++ scalatra-angularjs-app/project/plugins.sbt
@@ -1,12 +1,11 @@
-libraryDependencies += "com.github.siasia" %% "xsbt-web-plugin" % "0.12.0-0.2.11.1"
+addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "0.5.0")
 
-addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.0")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.2")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.6")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8")
 
-addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.0")
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
-addSbtPlugin("com.untyped" %% "sbt-js" % "0.5")
+addSbtPlugin("com.untyped" %% "sbt-js" % "0.6")
 
-addSbtPlugin("com.untyped" %% "sbt-less" % "0.5")
-
+addSbtPlugin("com.untyped" %% "sbt-less" % "0.6")
--- scalatra-angularjs-app/project/project/plugins.scala
+++ scalatra-angularjs-app/project/project/plugins.scala
@@ -1,7 +1,29 @@
+import java.io.File
 import sbt._
+import Keys._
 
-object plugins extends Build {
-
+object Plugins extends Build {
+  // Fixes SBT issue with git sources on Windows - Courtesy jodyfanning @ https://github.com/sbt/sbt/issues/895#issuecomment-29443827
   lazy val plugins = Project("plugins", file("."))
     .dependsOn(uri("git://github.com/guardian/sbt-jasmine-plugin.git#0.8"))
+
+  // Return our new resolver by default
+  override def buildLoaders =
+    BuildLoader.resolve(gitResolver) ::
+      Nil
+
+  // Define a new build resolver to wrap the original git one
+  def gitResolver(info: BuildLoader.ResolveInfo): Option[() => File] =
+    if (info.uri.getScheme != "git")
+      None
+    else {
+      // Use a subdirectory of the staging directory for the new plugin build.
+      // The subdirectory name is derived from a hash of the URI,
+      // and so identical URIs will resolve to the same directory.
+      val hashDir = new File(info.staging, Hash.halfHashString(info.uri.normalize.toASCIIString))
+      hashDir.mkdirs()
+
+      // Return the original git resolver that will do the actual work.
+      Resolvers.git(info)
+    }
 }
